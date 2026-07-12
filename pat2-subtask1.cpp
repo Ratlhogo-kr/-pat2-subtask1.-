d80b0f502c4252292ae4ca123b4458a5d3046a1c
unknown <202520048kr@gmail.comgit config --global user.name Kea>
2026-07-12 16:24:21 +0200

    adding morse code conversion

--- pat2-subtask1.cpp
+++ pat2-subtask1.cpp
@@ -2,3 +2,41 @@
 #include <string>
 #include <cctype>
 using namespace std;
+
+// Function to convert uppercase letters A-Z to Morse code
+string charToMorse(char c)
+{
+//Translates lowercase letters to uppercase
+	c = toupper(c);
+	
+	switch (c)
+	{
+		case'A':return".-";
+		case'B':return"-...";
+		case'C':return"-.-.";
+		case'D':return"-..";
+		case'E':return".";
+		case'F':return"..-.";
+		case'G':return"--.";
+		case'H':return"....";
+		case'I':return"..";
+		case'J':return".---";
+		case'K':return"-.-";
+		case'L':return".-..";
+		case'M':return"--";
+		case'N':return"-.";
+		case'O':return"---";
+		case'P':return".--.";
+		case'Q':return"--.-";
+		case'R':return".-.";
+		case'S':return"...";
+		case'T':return"-";
+		case'U':return"..-";
+		case'V':return"...-";
+		case'W':return"-..-";
+		case'Y':return"-.--";
+		case'Z':return"--..";
+		
+		default: return""; 
+	}
+}
